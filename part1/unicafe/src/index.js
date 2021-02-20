b8e98981bbb851af519bc252120b152d15d0ee69
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-02-21 00:57:13 +0100

    Exercise 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom'
 
 const rootElement = document.getElementById('root')
 
+const Statistics = ({good, neutral, bad}) => {
+
+    const average =  (good - bad) / (good + bad + neutral)
+    const positive =  good / (good + bad + neutral) * 100
+
+    return (
+        <div>
+            <h2>statistics</h2>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>average {average}</p>
+            <p>positive {positive} %</p>
+        </div>
+    )
+
+}
+
 const App = () => {
     // save clicks of each button to its own state
     const [good, setGood] = useState(0)
@@ -13,21 +31,13 @@ const App = () => {
     const handleNeutral = () => setNeutral(neutral + 1)
     const handleBad = () => setBad(bad + 1)
 
-    const average =  (good - bad) / (good + bad + neutral)
-    const positive =  good / (good + bad + neutral) * 100
-
     return (
         <div>
             <h1>give feedback</h1>
             <button onClick={handleGood}>good</button>
             <button onClick={handleNeutral}>neutral</button>
             <button onClick={handleBad}>bad</button>
-            <h2>statistics</h2>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>average {average}</p>
-            <p>positive {positive} %</p>
+            <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
     )
 }
