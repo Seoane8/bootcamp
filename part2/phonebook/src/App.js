d37b6788b35d03e5e3ca0e3cf3f870ad31106a28
Diego Seoane <diegoseoanegonzalez8@gmail.com>
2021-02-21 19:19:12 +0100

    Exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,14 +1,8 @@
-import React, {useState} from 'react'
-import {PersonForm} from "./PersonForm";
-import {Filter} from "./Filter";
-import {Persons} from "./Persons";
-
-const INITIAL_PERSONS = [
-    { name: 'Arto Hellas', tfno: '040-123456' },
-    { name: 'Ada Lovelace', tfno: '39-44-5323523' },
-    { name: 'Dan Abramov', tfno: '12-43-234345' },
-    { name: 'Mary Poppendieck', tfno: '39-23-6423122' }
-]
+import React, {useState, useEffect} from 'react'
+import {PersonForm} from "./PersonForm"
+import {Filter} from "./Filter"
+import {Persons} from "./Persons"
+import axios from 'axios'
 
 const INITIAL_NEW_PERSON = {
     name: '',
@@ -16,9 +10,20 @@ const INITIAL_NEW_PERSON = {
 }
 
 const App = () => {
-    const [persons, setPersons] = useState(INITIAL_PERSONS)
+    const [persons, setPersons] = useState([])
     const [newPerson, setNewPerson] = useState(INITIAL_NEW_PERSON)
     const [filter, setFilter] = useState('')
+
+    const getDataFromServer = () => {
+        return axios
+            .get('http://localhost:3001/persons')
+            .then(response => response.data)
+    }
+
+    useEffect(() => {
+        getDataFromServer()
+            .then(persons => setPersons(persons))
+    }, [])
 
 
     return (
@@ -36,10 +41,14 @@ const App = () => {
                 INITIAL_NEW_PERSON={INITIAL_NEW_PERSON}
             />
             <h2>Numbers</h2>
-            <Persons
-                data={persons}
-                filter={filter}
-            />
+            {persons.length === 0 ?
+                <p>Empty list</p> :
+                <Persons
+                    data={persons}
+                    filter={filter}
+                />
+            }
+
         </div>
     )
 }
